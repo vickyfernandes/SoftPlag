6d789cb0eedb821c11d21c98deccee06342c5e2a
Vicky Fernandes <vickyfernandes033@gmail.com>
2017-04-27 18:07:33 +0530

    minor changes

--- SoftPlag_v1/src/main.py
+++ SoftPlag_v1/src/main.py
@@ -8,19 +8,20 @@ Created on 25-Apr-2017
 from SoftPlag import *
 import os
 
-if __name__ == '__main__':
-    inputdir = "/home/mindcraft/Desktop/vicky/python/SoftPlag_v1/src/input/"
-    outputdir = "/home/mindcraft/Desktop/vicky/python/SoftPlag_v1/src/output/"
-    scpp=Pyscp()
-    scpj=Javascp()
-    filelist = os.listdir(inputdir)
+inputdir = "/home/mindcraft/Desktop/vicky/python/SoftPlag_v1/src/input/"
+outputdir = "/home/mindcraft/Desktop/vicky/python/SoftPlag_v1/src/output/"
+filelist = os.listdir(inputdir)
+
+if __name__ == '__main__':   
     for i in filelist:
         if i.endswith(".py") or i.endswith(".java"):  # You could also add "and i.startswith('f')
             with open(inputdir + i, 'r') as f:
                 fin = f.read()
-                #if i.endswith(".py"):
-                scpp.pscp(fin)
-                #else:
-                scpj.jscp(fin)
-    
-    
+                if i.endswith(".py"):
+                    scpp=Pyscp()    
+                    scpp.pscp(fin)
+                    f.close()
+                else:
+                    scpj=Javascp()
+                    scpj.jscp(fin)
+                    f.close()
